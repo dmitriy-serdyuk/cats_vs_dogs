6fbca5ee956bb8feb156b1a0770cdee7f081e801
dima <serdyuk.dmitriy@gmail.com>
2015-01-24 20:27:38 -0500

    Added dataset shuffling

--- cats_vs_dogs/make_dataset.py
+++ cats_vs_dogs/make_dataset.py
@@ -4,6 +4,7 @@ import os.path
 import cPickle as pkl
 
 import numpy as np
+import numpy.random
 
 from scipy import misc
 
@@ -19,13 +20,43 @@ def aggregate(directory, **kwargs):
                 pkl.dump((image, label), fout)
 
 
+def make_datasets(train_share, valid_share, directory, seed, **kwargs):
+    assert train_share + valid_share < 1.
+    all_files = []
+    for file in os.listdir(directory):
+        if file.endswith(".pkl"):
+            all_files += file
+    n_train = int(len(all_files) * train_share)
+    n_valid = int(len(all_files) * valid_share)
+
+    rng = np.random.RandomState(seed)
+    rng.shuffle(all_files)
+    train = all_files[:n_train]
+    valid = all_files[n_train:(n_train + n_valid)]
+    test = all_files[(n_train + n_valid):]
+    save_path = os.path.join(directory, '../datasets.pkl')
+    with open(save_path, 'w') as fout:
+        pkl.dump((train, valid, test), fout)
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory',
-                        default='/home/dima/Downloads/datasets/cats_vs_dogs/train/')
+                        default='/home/dima/Downloads/datasets/cats_vs_dogs/train/',
+                        help='Directory with data (extracted kaggle dataset)')
+    parser.add_argument('--train_share', type=float,
+                        default=0.6,
+                        help='Train dataset share')
+    parser.add_argument('--valid_share', type=float,
+                        default=0.2,
+                        help='Validation dataset share')
+    parser.add_argument('--seed', type=int,
+                        default=1,
+                        help='Random number generator seed')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
     aggregate(**args.__dict__)
+    make_datasets(**args.__dict__)
