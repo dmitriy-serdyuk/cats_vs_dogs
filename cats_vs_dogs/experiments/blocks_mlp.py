4b29ede5939038a658e5cd67c75fff33f7d0ea98
serdyuk <serdyuk.dmitriy@gmail.com>
2015-02-10 16:06:24 -0500

    Config file for experiments

--- cats_vs_dogs/experiments/blocks_mlp.py
+++ cats_vs_dogs/experiments/blocks_mlp.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import argparse
+import yaml
 
 import numpy
 
@@ -18,7 +19,7 @@ from blocks.algorithms import (GradientDescent, SteepestDescent, CompositeRule,
 from blocks.extensions import FinishAfter, Printing
 from blocks.extensions.monitoring import DataStreamMonitoring
 from blocks.extensions.saveload import SerializeMainLoop, LoadFromDump, Dump
-from cats_vs_dogs.extentions import DumpWeights, LoadWeights
+from blocks.config_parser import Configuration
 
 from ift6266h15.code.pylearn2.datasets.variable_image_dataset import RandomCrop
 
@@ -26,60 +27,69 @@ from cats_vs_dogs.iterators import DogsVsCats
 from cats_vs_dogs.bricks import Convolutional, Pooling, ConvNN
 from cats_vs_dogs.algorithms import Adam
 from cats_vs_dogs.schemes import SequentialShuffledScheme
+from cats_vs_dogs.extentions import DumpWeights, LoadWeights
 
 floatX = theano.config.floatX
 logging.basicConfig(level='INFO')
 
 
-def parse_args():
-    parser = argparse.ArgumentParser("Cats vs Dogs training algorithm")
-    parser.add_argument('--image-shape', type=int,
-                        default=221,
-                        help='Image shape')
-    parser.add_argument('--scaled-size', type=int,
-                        default=256,
-                        help='Scaled size')
-    parser.add_argument('--channels', type=int,
-                        default=3,
-                        help='Number of channels')
-    parser.add_argument('--batch-size', type=int,
-                        default=100,
-                        help='Batch size')
-    parser.add_argument('--epochs', type=int,
-                        default=50000,
-                        help='Number of epochs')
-    parser.add_argument('--load', action='store_true',
-                        default=False,
-                        help='Load the parameters')
-    parser.add_argument('--model-path',
+def parse_config(path):
+    config = ConfigCats()
+    config.add_config('image_shape', type_=int,
+                        default=221)
+    config.add_config('scaled_size', type_=int,
+                        default=256)
+    config.add_config('channels', type_=int,
+                        default=3)
+    config.add_config('batch_size', type_=int,
+                        default=100)
+    config.add_config('epochs', type_=int,
+                        default=50000)
+    config.add_config('load', type_=bool,
+                        default=False)
+    config.add_config('model_path', type_=str,
                         default='./models/model')
-    parser.add_argument('--use-adam', action='store_true',
-                        default=False,
-                        help='Use Adam optimizer')
-    parser.add_argument('--feature-maps', nargs='+', type=int,
-                        default=[25, 50, 100],
-                        help='Number of feature maps for layers')
-    parser.add_argument('--conv-sizes', nargs='+', type=int,
-                        default=[7, 5, 3],
-                        help='Convolution sizes for layers')
-    parser.add_argument('--pool-sizes', nargs='+', type=int,
-                        default=[3, 3, 3],
-                        help='Pooling sizes for layers')
-    parser.add_argument('--mlp-hiddens', nargs='+', type=int,
-                        default=[500],
-                        help='Number of hidden units in full connected layers')
-    return parser.parse_args()
+    config.add_config('use_adam', type_=bool,
+                        default=False)
+    config.add_config('feature_maps', type_=list,
+                        default=[25, 50, 100])
+    config.add_config('conv_sizes', type_=list,
+                        default=[7, 5, 3])
+    config.add_config('pool_sizes', type_=list,
+                        default=[3, 3, 3])
+    config.add_config('mlp_hiddens', type_=list,
+                        default=[500])
+    config.add_config('learning_rate', type_=float,
+                        default=1.e-4)
+    config.load_yaml(path)
+    return config
+
+
+class ConfigCats(Configuration):
+    def load_yaml(self, path):
+        yaml_file = os.path.expanduser(path)
+        if os.path.isfile(yaml_file):
+            with open(yaml_file) as f:
+                for key, value in yaml.safe_load(f).items():
+                    if key not in self.config:
+                        raise ValueError("Unrecognized config in YAML: {}"
+                                         .format(key))
+                    self.config[key]['yaml'] = value
 
 
 if __name__ == '__main__':
-    args = parse_args()
     logging.info('.. starting')
-    input_dim = (args.channels, args.image_shape, args.image_shape)
-    mlp_activations = [Rectifier() for _ in args.mlp_hiddens] + [Softmax()]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='config.yml')
+    args = parser.parse_args()
+    config = parse_config(args.config)
+
+    input_dim = (config.channels, config.image_shape, config.image_shape)
+    mlp_activations = [Rectifier() for _ in config.mlp_hiddens] + [Softmax()]
     model = ConvNN([Rectifier(), Rectifier()], input_dim,
-                   zip(args.feature_maps, args.conv_sizes, args.conv_sizes),
-                   zip(args.pool_sizes, args.pool_sizes),
-                   mlp_activations, args.mlp_hiddens + [2],
+                   zip(config.feature_maps, config.conv_sizes, config.conv_sizes),
+                   zip(config.pool_sizes, config.pool_sizes),
+                   mlp_activations, config.mlp_hiddens + [2],
                    weights_init=IsotropicGaussian(0.1),
                    biases_init=Constant(0.))
     model.initialize()
@@ -92,7 +102,7 @@ if __name__ == '__main__':
 
     logging.info('.. model built')
     rng = numpy.random.RandomState(2014 + 02 + 04)
-    transformer = RandomCrop(args.scaled_size, args.image_shape, rng)
+    transformer = RandomCrop(config.scaled_size, config.image_shape, rng)
     train_dataset = DogsVsCats('train', os.path.join('${PYLEARN2_DATA_PATH}',
                                                      'dogs_vs_cats',
                                                      'train.h5'),
@@ -100,7 +110,7 @@ if __name__ == '__main__':
     train_stream = DataStream(
         dataset=train_dataset,
         iteration_scheme=SequentialShuffledScheme(train_dataset.num_examples,
-                                          args.batch_size, rng))
+                                          config.batch_size, rng))
     test_dataset = DogsVsCats('test', os.path.join('${PYLEARN2_DATA_PATH}',
                                                    'dogs_vs_cats',
                                                    'train.h5'),
@@ -108,7 +118,7 @@ if __name__ == '__main__':
     test_stream = DataStream(
         dataset=test_dataset,
         iteration_scheme=SequentialScheme(train_dataset.num_examples,
-                                          args.batch_size))
+                                          config.batch_size))
     valid_dataset = DogsVsCats('valid', os.path.join('${PYLEARN2_DATA_PATH}',
                                                      'dogs_vs_cats',
                                                      'train.h5'),
@@ -116,7 +126,7 @@ if __name__ == '__main__':
     valid_stream = DataStream(
         dataset=valid_dataset,
         iteration_scheme=SequentialScheme(train_dataset.num_examples,
-                                          args.batch_size))
+                                          config.batch_size))
 
     train_monitor = DataStreamMonitoring(
         variables=[cost, error_rate], data_stream=train_stream, prefix="train")
@@ -126,22 +136,22 @@ if __name__ == '__main__':
         variables=[cost, error_rate], data_stream=test_stream, prefix="test")
 
     extensions = []
-    if args.load:
-        extensions += [LoadWeights(args.model_path)]
-    extensions += [FinishAfter(after_n_epochs=args.epochs),
+    if config.load:
+        extensions += [LoadWeights(config.model_path)]
+    extensions += [FinishAfter(after_n_epochs=config.epochs),
                    train_monitor,
                    valid_monitor,
                    test_monitor,
                    SerializeMainLoop('./models/main.pkl'),
                    Printing(),
-                   DumpWeights(args.model_path, after_every_epoch=True,
+                   Dump(config.model_path, after_every_epoch=True,
                                before_first_epoch=True)]
 
-    if args.use_adam:
+    if config.use_adam:
         step_rule = Adam()
     else:
         step_rule = CompositeRule([GradientClipping(threshold=numpy.cast[floatX](1000.)),
-                                   SteepestDescent(learning_rate=1.e-5)])
+                                   SteepestDescent(learning_rate=config.learning_rate)])
     main_loop = MainLoop(
         model, data_stream=train_stream,
         algorithm=GradientDescent(
